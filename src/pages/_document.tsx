769e3a006789d31da3b6e09561a990386a512bec
dosazuwa <dsazuwa@gmail.com>
2024-03-03 19:06:31 +0100

    change font to Inter

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
-          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'
+          rel='stylesheet'
         />
       </Head>
 
